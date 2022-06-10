55d273de4d39c7fc87dfa3b8f6b0182c12f32b5e
Lucca Ramalho <luccaramalho@ibict.br>
2022-06-10 11:31:50 -0300

    Alterando arquivo com total de documentos para localhost

--- functions/total_de_documentos.R
+++ functions/total_de_documentos.R
@@ -1,2 +1,2 @@
-total_de_documentos <- fromJSON("https://localhost.ibict.br/vufind/api/v1/search?&type=AllFields&page=0&limit=0&sort=relevance")
+total_de_documentos <- fromJSON("http://localhost.ibict.br/vufind/api/v1/search?&type=AllFields&page=0&limit=0&sort=relevance")
 total_de_documentos <- total_de_documentos$resultCount
