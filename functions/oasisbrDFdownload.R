d3a2dac31be1f22e44a5c6da98db36b9eea63334
Lucca Ramalho <luccaramalho@ibict.br>
2022-06-21 15:30:09 -0300

    Alterando funcao de download de DF para localhost

--- functions/oasisbrDFdownload.R
+++ functions/oasisbrDFdownload.R
@@ -1,1 +1,3 @@
 oasisbrDF <- fromJSON("http://localhost/vufind/api/v1/search?&type=AllFields&page=0&limit=0&sort=relevance&facet[]=author_facet&facet[]=dc.subject.por.fl_str_mv&facet[]=eu_rights_str_mv&facet[]=dc.publisher.program.fl_str_mv&facet[]=dc.subject.cnpq.fl_str_mv&facet[]=publishDate&facet[]=language&facet[]=format&facet[]=institution&facet[]=dc.contributor.advisor1.fl_str_mv")
+
+View(oasisbrDF)
